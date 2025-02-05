2a187880d925aac003ea4d38ed2f6cfaf6a4817d
MIF <muhammadvvn0@gmail.com>
2025-02-05 13:29:05 +0200

    fix somting in this project

--- main.js
+++ main.js
@@ -1,34 +1,24 @@
 // Step One Get All Eelement
-
 let hrs = document.getElementById("hrs");
 let min = document.getElementById("min");
 let sec = document.getElementById("sec");
-// 
-const audio = document.getElementById('backgroundAudio');
+const musicIcon = document.getElementById("icon");
+const music = document.getElementById("backgroundAudio");
+// [2]
+setInterval(function () {
+  let dateTime = new Date();
+  hrs.innerHTML = (dateTime.getHours() < 10 ? "0" : "") + dateTime.getHours();
+  min.innerHTML =
+    (dateTime.getMinutes() < 10 ? "0" : "") + dateTime.getMinutes();
+  sec.innerHTML =
+    (dateTime.getSeconds() < 10 ? "0" : "") + dateTime.getSeconds();
+}, 1000);
 
+//[3]
+musicIcon.addEventListener("click", function () {
+  music.play();
+});
 
-// Step Two Function Date();  and Using Methods This Function
+let dateTime2 = new Date();
 
-// let dateTime = new Date();
-
-// hrs.innerHTML = dateTime.getHours();
-// min.innerHTML = dateTime.getMinutes();
-// sec.innerHTML = dateTime.getSeconds();
-
-// Up data date step three
-
-setInterval(function() {
-
-    let dateTime = new Date();
-
-hrs.innerHTML = (dateTime.getHours() < 10 ? "0" : "") + dateTime.getHours();
-min.innerHTML = (dateTime.getMinutes() < 10 ? "0" : "") + dateTime.getMinutes();
-sec.innerHTML = (dateTime.getSeconds() < 10 ? "0" : "") + dateTime.getSeconds();
-
-} , 1000)
-
-// 
-
-document.body.addEventListener('click', function() {
-    audio.play(); 
-  }); 
+console.log(dateTime2);
